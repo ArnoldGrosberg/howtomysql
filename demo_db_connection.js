96ea077db125e1984cbef1690896b29019a4c928
Arnold Grosberg <arnold.grosberg@khk.ee>
2022-02-07 11:00:20 +0200

    Created a connection

---
+++ demo_db_connection.js
@@ -0,0 +1,24 @@
+var mysql = require('mysql2');
+
+
+
+var con = mysql.createConnection({
+
+  host: "localhost",
+
+  user: "root",
+
+  password: "qwerty"
+
+});
+
+
+
+con.connect(function(err) {
+
+  if (err) throw err;
+
+  console.log("Connected!");
+
+});
+
